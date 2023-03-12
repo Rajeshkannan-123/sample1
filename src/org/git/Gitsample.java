db264cf3edc64175b5b7e549367b83e1fa967de9
Rajeshkannan-123 <rajeshkannanm.citmech2017@gamil.com>
2023-03-12 23:13:07 +0530

    sample1

---
+++ src/org/git/Gitsample.java
@@ -0,0 +1,24 @@
+package org.git;
+
+public class Gitsample {
+	
+	public static void main(String[] args) {
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		System.out.println(1234);
+		
+	}
+
+}
